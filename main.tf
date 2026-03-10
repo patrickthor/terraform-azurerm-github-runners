6e09583b08a241ac947a1df448041f2edce3c412
patrickthor <patrick.thoor@gmail.com>
2026-03-10 08:53:50 +0100

    fix: revert KV network ACL to Allow - Deny blocks local access and is incompatible with current setup

--- main.tf
+++ main.tf
@@ -81,7 +81,7 @@ resource "azurerm_key_vault" "kv" {
   rbac_authorization_enabled      = true
 
   network_acls {
-    default_action = "Deny"
+    default_action = "Allow"
     bypass         = "AzureServices"
   }
 
